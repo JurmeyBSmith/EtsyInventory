59376c70988da5fbd13524f6cc2f9f6464963a21
JBrennanSmith <j.b.s.speechdebate@gmail.com>
2021-09-02 21:02:38 -0700

    Base Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Fuck youuuu leroooooy!.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {}
+  handleItemClick = (e, {name}) => this.setState({activeItem: name }) 
+   
+  render() {
+    return (
+
+      <Router>
+        <div>
+          <Route path="/" component={Home} />
+        </div>
+        
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+//import { Header } from 'semantic-ui-react'
+
+
+class Home extends Component {
+
+
+    render() {
+        return (
+            <header className="App-header">
+                <h1>Welcome to Etsy Inventory!</h1>
+                <h3>Cool shit coming soon...</h3>
+            </header>
+        )
+    }
+}
+export default Home;
